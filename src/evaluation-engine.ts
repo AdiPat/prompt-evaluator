d7747e4dbbe8d25a94d67376fc0a4e7036c2eb4d
Aditya Patange <contact.adityapatange@gmail.com>
2024-12-01 19:34:39 +0530

    evaluation-engine: generate object call in evaluatePrompt

--- src/evaluation-engine.ts
+++ src/evaluation-engine.ts
@@ -1,9 +1,20 @@
+import { ai } from "./ai";
+import { simplePromptEvalSchema } from "./models";
+
 export class EvaluationEngine {
-  evaluatePrompt({ prompt }: { prompt: string }) {
+  async evaluatePrompt({ prompt }: { prompt: string }) {
+    const { object: evaluationResult } = await ai.generateObject({
+      model: ai.models.gpt_4o_mini,
+      system:
+        "You are an LLM-judge. Given a prompt, provide a score on 'how good' the prompt is. The score should be between 0 and 1.",
+      prompt: `Prompt: ${prompt}`,
+      schema: simplePromptEvalSchema,
+    });
+
     return {
       prompt,
-      score: 0,
-      feedback: "Not implemented",
+      score: evaluationResult.score,
+      feedback: evaluationResult.feedback,
     };
   }
 }
--- src/tests/evaluation-engine.test.ts
+++ src/tests/evaluation-engine.test.ts
@@ -1,18 +1,45 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach, vi } from "vitest";
 import { EvaluationEngine } from "../evaluation-engine";
+import { ai } from "../ai";
+import { afterEach } from "node:test";
+import { simplePromptEvalSchema } from "../models";
 
 describe("evaluation engine", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
   describe("evaluatePrompt should", () => {
-    it("should return response of valid format", () => {
+    it("should return response of valid format", async () => {
       const evaluationEngine = new EvaluationEngine();
       const prompt = "What is the meaning of life?";
-      const evaluationResult = evaluationEngine.evaluatePrompt({
+      const evaluationResult = await evaluationEngine.evaluatePrompt({
         prompt,
       });
       expect(evaluationResult).toEqual({
         prompt,
         score: expect.any(Number),
         feedback: expect.any(String),
+      });
+    });
+
+    it("should call generateObject with correct parameters", async () => {
+      const generateObjectSpy = vi.spyOn(ai, "generateObject");
+      const evaluationEngine = new EvaluationEngine();
+      const prompt = "What is the meaning of life?";
+      await evaluationEngine.evaluatePrompt({
+        prompt,
+      });
+      expect(generateObjectSpy).toHaveBeenCalledWith({
+        model: ai.models.gpt_4o_mini,
+        system:
+          "You are an LLM-judge. Given a prompt, provide a score on 'how good' the prompt is. The score should be between 0 and 1.",
+        prompt: `Prompt: ${prompt}`,
+        schema: simplePromptEvalSchema,
       });
     });
   });
